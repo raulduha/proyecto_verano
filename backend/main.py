c0a1d33219543e779045f93cfe85b2401b0afa81
raulduha <80332171+raulduha@users.noreply.github.com>
2022-12-12 16:55:37 -0300

    first commit

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hola")
+def hola_mundo():
+    return{"Hola":"Mundo"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str,None]= None):
+    return {'item_id': item_id, 'q':q}
+
+@app.put("/items/{item_id}") #actualizar
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/calculadora")
+def calcular(operando1: float, operando2: float):
+    return {'suma': operando1 + operando2}
